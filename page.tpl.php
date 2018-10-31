eb15e0354127aea834795006b0c4df3ec5536dac
omatseye <40199419+omatseye@users.noreply.github.com>
2018-10-31 14:41:34 +0100

    git remote add origin https://github.com/omatseye/opencharity.git

---
+++ page.tpl.php
@@ -0,0 +1,70 @@
+<?php
+/** This theme was develop from scratch by Clarke Peter
+*/
+?>
+<!DOCTYPE html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="https://www.opencharityuk.org/sites/all/themes/cc_test/favicon.ico" type="image/vnd.microsoft.icon" />
+<meta name="Generator" content="Drupal 7 (http://drupal.org)" />
+<link rel="alternate" type="application/rss+xml" title="Open Charity RSS" href="https://www.opencharityuk.org/rss.xml" />
+<title>Open Charity</title>
+<style type="text/css" media="all">
+@import url("https://www.opencharityuk.org/modules/system/system.base.css?pf8qj3");
+@import url("https://www.opencharityuk.org/modules/system/system.menus.css?pf8qj3");
+@import url("https://www.opencharityuk.org/modules/system/system.messages.css?pf8qj3");
+@import url("https://www.opencharityuk.org/modules/system/system.theme.css?pf8qj3");
+</style>
+<style type="text/css" media="all">
+@import url("https://www.opencharityuk.org/modules/comment/comment.css?pf8qj3");
+@import url("https://www.opencharityuk.org/sites/all/modules/date/date_api/date.css?pf8qj3");
+@import url("https://www.opencharityuk.org/sites/all/modules/date/date_popup/themes/datepicker.1.7.css?pf8qj3");
+@import url("https://www.opencharityuk.org/modules/field/theme/field.css?pf8qj3");
+@import url("https://www.opencharityuk.org/modules/node/node.css?pf8qj3");
+@import url("https://www.opencharityuk.org/modules/search/search.css?pf8qj3");
+@import url("https://www.opencharityuk.org/modules/user/user.css?pf8qj3");
+@import url("https://www.opencharityuk.org/sites/all/modules/views/css/views.css?pf8qj3");
+@import url("https://www.opencharityuk.org/sites/all/modules/contrib/ckeditor/css/ckeditor.css?pf8qj3");
+</style>
+<style type="text/css" media="all">
+@import url("https://www.opencharityuk.org/sites/all/modules/ctools/css/ctools.css?pf8qj3");
+@import url("https://www.opencharityuk.org/sites/all/modules/panels/css/panels.css?pf8qj3");
+</style>
+<style type="text/css" media="all">
+@import url("https://www.opencharityuk.org/sites/all/themes/cc_test/dist/main.min.css?pf8qj3");
+</style>
+<script type="text/javascript" src="https://www.opencharityuk.org/sites/all/modules/jquery_update/replace/jquery/1.10/jquery.min.js?v=1.10.2"></script>
+<script type="text/javascript" src="https://www.opencharityuk.org/misc/jquery.once.js?v=1.2"></script>
+<script type="text/javascript" src="https://www.opencharityuk.org/misc/drupal.js?pf8qj3"></script>
+<script type="text/javascript" src="https://www.opencharityuk.org/sites/all/modules/contrib/google_analytics/googleanalytics.js?pf8qj3"></script>
+<script type="text/javascript">
+<!--//--><![CDATA[//><!--
+(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");ga("create", "UA-310745-8", {"cookieDomain":"auto"});ga("set", "anonymizeIp", true);ga("send", "pageview");
+//--><!]]>
+</script>
+<script type="text/javascript" src="https://www.opencharityuk.org/sites/all/themes/cc_test/dist/main.min.js?pf8qj3"></script>
+<script type="text/javascript">
+<!--//--><![CDATA[//><!--
+jQuery.extend(Drupal.settings, {"basePath":"\/","pathPrefix":"","ajaxPageState":{"theme":"cc_test","theme_token":"21PVctTH-XM_oLGc0mlZWhQqGlpnymgQKdwnBmBlBCw","js":{"sites\/all\/modules\/jquery_update\/replace\/jquery\/1.10\/jquery.min.js":1,"misc\/jquery.once.js":1,"misc\/drupal.js":1,"sites\/all\/modules\/contrib\/google_analytics\/googleanalytics.js":1,"0":1,"sites\/all\/themes\/cc_test\/dist\/main.min.js":1},"css":{"modules\/system\/system.base.css":1,"modules\/system\/system.menus.css":1,"modules\/system\/system.messages.css":1,"modules\/system\/system.theme.css":1,"modules\/comment\/comment.css":1,"sites\/all\/modules\/date\/date_api\/date.css":1,"sites\/all\/modules\/date\/date_popup\/themes\/datepicker.1.7.css":1,"modules\/field\/theme\/field.css":1,"modules\/node\/node.css":1,"modules\/search\/search.css":1,"modules\/user\/user.css":1,"sites\/all\/modules\/views\/css\/views.css":1,"sites\/all\/modules\/contrib\/ckeditor\/css\/ckeditor.css":1,"sites\/all\/modules\/ctools\/css\/ctools.css":1,"sites\/all\/modules\/panels\/css\/panels.css":1,"sites\/all\/themes\/cc_test\/dist\/main.min.css":1}},"googleanalytics":{"trackOutbound":1,"trackMailto":1,"trackDownload":1,"trackDownloadExtensions":"7z|aac|arc|arj|asf|asx|avi|bin|csv|doc(x|m)?|dot(x|m)?|exe|flv|gif|gz|gzip|hqx|jar|jpe?g|js|mp(2|3|4|e?g)|mov(ie)?|msi|msp|pdf|phps|png|ppt(x|m)?|pot(x|m)?|pps(x|m)?|ppam|sld(x|m)?|thmx|qtm?|ra(m|r)?|sea|sit|tar|tgz|torrent|txt|wav|wma|wmv|wpd|xls(x|m|b)?|xlt(x|m)|xlam|xml|z|zip"}});
+//--><!]]>
+</script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+
+<div id="page-wrapper"><div id="page">
+<head>
+   <div id="logo"> </div>
+   <nav> </nav>
+</header>
+
+<div id="slideshow"><?php print render($page['slideshow']);?> </div>
+
+<div id="slideshow"><?php print render($page['slideshow']);?> </div>
+
+<article><?php print render($page['content']);?> </article>
+
+<footer><?php print render($page['footer']);?> </footer>
+
+</div>
